ccb8eb1c8d420fa8a68e7e489ff894dc663bc4e5
Dylan Roscover <rosco@tec.design>
2022-12-26 12:09:22 -0800

    better init for blank projects / tox drag-n-drop

--- src/lib/project1/Embody/externalizations.py
+++ src/lib/project1/Embody/externalizations.py
@@ -1,12 +1,11 @@
 network_path	type	rel_file_path	timestamp	dirty
-/project1/Embody/externalizations	table	lib/project1/Embody/externalizations.py	2022-12-26 02:50:33	
-/text_root	text	lib/text_root.py	2022-12-26 02:55:15	
-/project1/base1	base	lib/project1/base1.tox	2022-12-26 02:55:54	False
-/project1/base3	base	lib/project1/base3.tox	2022-12-26 02:56:07	False
-/project1/base4	base	lib/project1/base4.tox	2022-12-26 02:56:07	False
-/project1/base2moved	base	lib/project1/base2moved.tox	2022-12-26 02:56:07	False
-/project1/sdf	text	lib/project1/sdf.py	2022-12-26 02:56:07	
-/project1/text2	text	lib/project1/text2.py	2022-12-26 02:56:07	
-/project1/text3	text	lib/project1/text3.py	2022-12-26 02:56:07	
-/project1/text2_xml	text	lib/project1/text2_xml.py	2022-12-26 02:56:07	
-/project1/text_json	text	lib/project1/text_json.py	2022-12-26 02:56:07	
+/project1/base1	base	lib/project1/base1.tox	2022-12-26 08:07:50	False
+/project1/base3	base	lib/project1/base3.tox	2022-12-26 08:07:50	False
+/project1/base4	base	lib/project1/base4.tox	2022-12-26 08:07:50	False
+/project1/base2moved	base	lib/project1/base2moved.tox	2022-12-26 08:07:50	False
+/project1/sdf	text	lib/project1/sdf.py	2022-12-26 08:07:50	
+/project1/text2	text	lib/project1/text2.py	2022-12-26 08:07:50	
+/project1/text3	text	lib/project1/text3.py	2022-12-26 08:07:50	
+/project1/text2_xml	text	lib/project1/text2_xml.py	2022-12-26 08:07:50	
+/project1/text_json	text	lib/project1/text_json.py	2022-12-26 08:07:50	
+/text_root	text	lib/text_root.py	2022-12-26 08:07:50	
